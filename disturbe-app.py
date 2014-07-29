39a8e716eb31efb74501423b45c8ec5c862e372e
Marius Cobzarenco <marius@reinfer.io>
2014-07-29 16:44:42 +0100

    implemented a CurveCP-like JSON stateless protocol for client authentication

--- disturbe-app.py
+++ disturbe-app.py
@@ -1,29 +1,348 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division
-
 import argparse
+import inspect
 import json
+import logging
+import math
 import re
 import sys
 
+import jsonschema
 import riak
+import nacl.utils
+from nacl.encoding import URLSafeBase64Encoder as Base64Encoder
+from nacl.public import PrivateKey, PublicKey, Box
+from nacl.secret import SecretBox
+from nacl.exceptions import CryptoError
+
 import bottle
 from bottle import Bottle, route, template, static_file, request, view, \
-    post, redirect, error, abort
+    post, redirect, error, abort, HTTPError, PluginError
 
+
+HTTP_BAD_REQUEST = 400
+HTTP_INTERNAL_SERVER_ERROR = 500
 
 DEFAULT_BIND = '127.0.0.1:8080'
 DEFAULT_RIAK = ['127.0.0.1:8087']
 DEFAULT_WORKERS = 4
 
-DISTURBE_HTML = 'disturbe.html'
+DISTURBE_INDEX = 'disturbe.html'
 
+CURVE25519_KEY_BYTES = 32
+SERVER_PUBLIC_KEY = 'kC_rSIO7t1ryhux1sn_LrtTrLyVZNd08BCXnSHQjgmA='
+SERVER_PRIVATE_KEY = 'qc_YLigJ6Sm7loGoipLQx0KZfBPqOpmZfxcoHCRBPUI='
+
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s] %(message)s',
+                    datefmt="%Y-%m-%d %H:%M:%S")
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
+# TODO: (mcobzarenco) replace with redis
+minute_keys = {}
+
+
+class InvalidClientRequest(Exception):
+    """ Signals an invalid client request """
+
+
+class BottlePlugin(object):
+    def __init__(self, plugin_name, keyword, obj):
+        self._name = plugin_name
+        self._obj = obj
+        self._keyword = keyword
+        self.api = 2
+
+    def setup(self, app):
+        ''' Make sure that other installed plugins don't affect the same
+            keyword argument.'''
+        for other in app.plugins:
+            if hasattr(other, '_keyword') and other._keyword == self._keyword:
+                raise PluginError("Found another plugin with " \
+                                  "conflicting settings (non-unique keyword).")
+
+    def apply(self, callback, route):
+        # Override global configuration with route-specific values
+        conf = route.app.config.get(self._name) or {}
+        keyword = conf.get('keyword', self._keyword)
+
+        # Test if the original callback accepts the required keyword
+        # Ignore it if it does not need a handler to _obj
+        args = inspect.getargspec(route.callback)[0]
+        if keyword not in args:  return callback
+
+        def wrapper(*args, **kwargs):
+            kwargs[keyword] = self._obj
+            rv = callback(*args, **kwargs)
+            return rv
+        # Replace the route callback with the wrapped one
+        return wrapper
+
+
+class RiakKey(str):
+    """Workaround necessary because the Riak client
+    idiotically doesn't support binary keys"""
+    def encode(self, ignored):
+        return self
+
+
+def parse_and_verify_json(json_string, schema):
+    try:
+        parsed = json.loads(json_string)
+        jsonschema.validate(parsed, schema)
+        return parsed
+    except jsonschema.ValidationError as e:
+        raise InvalidClientRequest(e)
+    except ValueError as e:
+        raise InvalidClientRequest('Invalid JSON in the request.')
+
+
+def expect_json_request(request, schema=None):
+    schema = schema or {}
+    if 'application/json' not in request.content_type:
+        raise InvalidClientRequest(
+            'content-type was "%s", it should be "application/json"' %
+            request.content_type)
+    return parse_and_verify_json(request._get_body_string(), schema)
+
+
+#######                           Routes                               #######
+
+
+BASE64_REGEX = r'^(?:[A-Za-z0-9\-_]{4})*(?:[A-Za-z0-9\-_]{2}==|[A-Za-z0-9\-_]{3}=)?$'
+
+HELLO_PADDING_BYTES = 64
+HELLO_PADDING_BASE64_BYTES = int(math.ceil(HELLO_PADDING_BYTES / 3.0)) * 4
+HELLO_CLIENT_TRANSIENT_PKEY_FIELD = 'client_tpkey'
+HELLO_PADDING_FIELD = 'padding'
+HELLO_ZEROS_BOX_FIELD = 'zeros_box'
+
+HELLO_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        HELLO_CLIENT_TRANSIENT_PKEY_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        },
+        HELLO_PADDING_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX,
+            'minLength': HELLO_PADDING_BASE64_BYTES,
+            'maxLength': HELLO_PADDING_BASE64_BYTES
+        },
+        HELLO_ZEROS_BOX_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        }
+    },
+    'additionalProperties': False,
+    'required': [HELLO_CLIENT_TRANSIENT_PKEY_FIELD,
+                 HELLO_PADDING_FIELD,
+                 HELLO_ZEROS_BOX_FIELD]
+}
+
+COOKIE_SERVER_TRANSIENT_PKEY_FIELD = 'server_tpkey'
+COOKIE_COOKIE_FIELD = 'cookie'
+COOKIE_COOKIE_BASE64_BYTES = 140
+COOKIE_COOKIE_BOX_FIELD = 'cookie_box'
+
+COOKIE_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        COOKIE_COOKIE_BOX_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        },
+    },
+    'additionalProperties': False,
+    'required': [COOKIE_COOKIE_BOX_FIELD]
+}
+
+INITIATE_CLIENT_TRANSIENT_PKEY_FIELD = HELLO_CLIENT_TRANSIENT_PKEY_FIELD
+INITIATE_COOKIE_FIELD = 'cookie'
+INITIATE_COOKIE_BASE64_BYTES = COOKIE_COOKIE_BASE64_BYTES
+INITIATE_VOUCH_FIELD = 'vouch'
+
+INITIATE_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        INITIATE_CLIENT_TRANSIENT_PKEY_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        },
+        INITIATE_COOKIE_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX,
+            'minLength': INITIATE_COOKIE_BASE64_BYTES,
+            'maxLength': INITIATE_COOKIE_BASE64_BYTES
+        },
+        INITIATE_VOUCH_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        }
+    },
+    'additionalProperties': False,
+    'required': [INITIATE_CLIENT_TRANSIENT_PKEY_FIELD,
+                 INITIATE_COOKIE_FIELD,
+                 INITIATE_VOUCH_FIELD]
+}
+
+VOUCH_CLIENT_PKEY_FIELD = 'client_pkey'
+VOUCH_TRANSIENT_KEY_BOX_FIELD = 'transient_key_box'
+VOUCH_DOMAIN_NAME_FIELD = 'domain_name'
+VOUCH_MESSAGE_FIELD = 'message'
+
+VOUCH_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        VOUCH_CLIENT_PKEY_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX
+        },
+        VOUCH_TRANSIENT_KEY_BOX_FIELD: {
+            'type': 'string',
+            'pattern': BASE64_REGEX,
+        },
+        VOUCH_DOMAIN_NAME_FIELD: {
+            'type': 'string'
+        },
+        VOUCH_MESSAGE_FIELD: {
+            'type': 'object'
+        }
+    },
+    'additionalProperties': False,
+    'required': [VOUCH_CLIENT_PKEY_FIELD,
+                 VOUCH_TRANSIENT_KEY_BOX_FIELD,
+                 VOUCH_DOMAIN_NAME_FIELD,
+                 VOUCH_MESSAGE_FIELD]
+}
+
+
+def open_box(nonceCipher, private_key, public_key, encoder=Base64Encoder):
+    box = Box(private_key, public_key)
+    return box.decrypt(str(nonceCipher), encoder=encoder)
+
+
+def check_exact_length(value, value_name, expected_len):
+        if len(value) != expected_len:
+            raise InvalidClientRequest('%s should be exactly %d bytes long' %
+                                       (value_name, expected_len))
 
 @route('/')
 @route('/<filepath:path>')
-def server_static(filepath=DISTURBE_HTML):
+def server_static(filepath=DISTURBE_INDEX):
     return static_file(filepath, root='static/')
+
+
+@route('/handshake/hello', method='POST')
+def handshake_hello(private_key):
+    try:
+        request = expect_json_request(bottle.request, HELLO_SCHEMA)
+        client_transient_pkey = PublicKey(
+            str(request[HELLO_CLIENT_TRANSIENT_PKEY_FIELD]), Base64Encoder)
+
+        zeros = open_box(request[HELLO_ZEROS_BOX_FIELD],
+                         private_key, client_transient_pkey)
+        if len(zeros) != HELLO_PADDING_BYTES:
+            raise InvalidClientRequest(
+                'zeros_box should contain exactly %d bytes of padding' %
+                HELLO_PADDING_BYTES)
+
+        transient_skey = PrivateKey.generate()
+        cookie_plain = client_transient_pkey.encode() + \
+                       transient_skey.encode()
+        cookie_nonce = nacl.utils.random(SecretBox.NONCE_SIZE)
+        symmetric_key = nacl.utils.random(SecretBox.KEY_SIZE)
+        cookie_sbox = SecretBox(symmetric_key)
+        cookie = cookie_sbox.encrypt(
+            cookie_plain, cookie_nonce, encoder=Base64Encoder)
+        minute_keys[cookie] = symmetric_key
+        # print(minute_keys)
+
+        cookie_box = Box(private_key, client_transient_pkey)
+        cookie_box_nonce = nacl.utils.random(Box.NONCE_SIZE)
+        server_tpkey = transient_skey.public_key.encode(Base64Encoder)
+        cookie_box_cipher = cookie_box.encrypt(json.dumps({
+            COOKIE_SERVER_TRANSIENT_PKEY_FIELD: server_tpkey,
+            COOKIE_COOKIE_FIELD: cookie
+        }), cookie_box_nonce, encoder=Base64Encoder)
+
+        response = {COOKIE_COOKIE_BOX_FIELD: cookie_box_cipher}
+        jsonschema.validate(response, COOKIE_SCHEMA)
+        return response
+    except jsonschema.ValidationError:
+        log.exception(e)
+        bottle.response.status = HTTP_INTERNAL_SERVER_ERROR
+        return {'error': 'A packet with an invalid JSON schema was generated.'}
+    except InvalidClientRequest as e:
+        log.exception(e)
+        bottle.response.status = HTTP_BAD_REQUEST
+        return {'error': str(e)}
+    except CryptoError as e:
+        log.exception(e)
+        bottle.response.status = HTTP_BAD_REQUEST
+        return {'error': 'bad encryption'}
+    return {'error': ''}
+
+
+@route('/handshake/initiate', method='POST')
+def handshake_initiate(private_key):
+    try:
+        request = expect_json_request(bottle.request, INITIATE_SCHEMA)
+
+        symmetric_key = minute_keys[request[INITIATE_COOKIE_FIELD]]
+        cookie_sbox = SecretBox(symmetric_key)
+        cookie = cookie_sbox.decrypt(
+            str(request[INITIATE_COOKIE_FIELD]), encoder=Base64Encoder)
+        del minute_keys[request[INITIATE_COOKIE_FIELD]]
+
+        if len(cookie) != 2 * CURVE25519_KEY_BYTES:
+            bottle.response.status = HTTP_INTERNAL_SERVER_ERROR
+            return {'error': 'An invalid cookie was sent to the client.'}
+        client_transient_pkey = PublicKey(cookie[0:CURVE25519_KEY_BYTES])
+        transient_skey = PrivateKey(cookie[CURVE25519_KEY_BYTES:])
+
+        if request[INITIATE_CLIENT_TRANSIENT_PKEY_FIELD] != \
+           client_transient_pkey.encode(Base64Encoder):
+            raise InvalidClientRequest(
+                'Initiate: non matching transient public keys.')
+
+        vouch_json = open_box(request[INITIATE_VOUCH_FIELD],
+                              transient_skey, client_transient_pkey)
+        vouch = parse_and_verify_json(vouch_json, VOUCH_SCHEMA)
+
+        client_pkey = PublicKey(
+            str(vouch[VOUCH_CLIENT_PKEY_FIELD]), encoder=Base64Encoder)
+        vouch_for_transient_pkey = open_box(
+            vouch[VOUCH_TRANSIENT_KEY_BOX_FIELD], private_key, client_pkey)
+        if vouch_for_transient_pkey != client_transient_pkey.encode():
+            raise InvalidClientRequest(
+                'Initiate: non matching transient public keys.')
+
+        resp = 'I believe you are %s' % client_pkey.encode(Base64Encoder)
+        print(resp)
+        response_nonce = nacl.utils.random(Box.NONCE_SIZE)
+        response_box = Box(transient_skey, client_transient_pkey)
+        response_box_cipher = response_box.encrypt(
+            resp, response_nonce, encoder=Base64Encoder)
+        return {'response': response_box_cipher}
+    except jsonschema.ValidationError as e:
+        log.exception(e)
+        bottle.response.status = HTTP_BAD_REQUEST
+        return {'error': str(e)}
+    except InvalidClientRequest as e:
+        log.exception(e)
+        bottle.response.status = HTTP_BAD_REQUEST
+        return {'error': str(e)}
+    except CryptoError as e:
+        log.exception(e)
+        bottle.response.status = HTTP_BAD_REQUEST
+        return {'error': 'Bad encryption in handshake.'}
+    return {'error': ''}
 
 
 def parse_hostport(hostport):
@@ -58,6 +377,16 @@ if __name__ == '__main__' or True:
 
     bind = parse_hostport(args.bind)
     app = bottle.app()
+
+    riak_nodes = []
+    for hostport in args.riak:
+        node = parse_hostport(hostport)
+        riak_nodes.append({'host': node['host'], 'pb_port': node['port']})
+    riak_client = riak.RiakClient(protocol='pbc', nodes=riak_nodes)
+    app.install(BottlePlugin('riak_client', 'riak_client', riak_client))
+
+    private_key = PrivateKey(SERVER_PRIVATE_KEY, Base64Encoder)
+    app.install(BottlePlugin('private_key', 'private_key', private_key))
 
     if args.debug:
         bottle.run(app=app, host=bind['host'], port=bind['port'],
--- static/disturbe.coffee
+++ static/disturbe.coffee
@@ -1,10 +1,38 @@
 {a, br, button, div, form, hr, h1, h2, h3, h4, h5, h6, i, input,
   label, li, p, option, select, span, strong, ul} = React.DOM
 
+
+### Json CurveCP Protocol Constants ###
+
+SERVER_PUBLIC_KEY = 'kC_rSIO7t1ryhux1sn_LrtTrLyVZNd08BCXnSHQjgmA='
+SERVER_DOMAIN_NAME = 'curvech.at'
+
+HELLO_URL = '/handshake/hello'
+HELLO_PADDING_BYTES = 64
+HELLO_CLIENT_TRANSIENT_PKEY_FIELD = 'client_tpkey'
+HELLO_PADDING_FIELD = 'padding'
+HELLO_ZEROS_BOX_FIELD = 'zeros_box'
+
+INITIATE_URL = '/handshake/initiate'
+INITIATE_CLIENT_TRANSIENT_PKEY_FIELD = HELLO_CLIENT_TRANSIENT_PKEY_FIELD
+INITIATE_COOKIE_FIELD = 'cookie'
+INITIATE_VOUCH_FIELD = 'vouch'
+
+VOUCH_CLIENT_PKEY_FIELD = 'client_pkey'
+VOUCH_TRANSIENT_KEY_BOX_FIELD = 'transient_key_box'
+VOUCH_DOMAIN_NAME_FIELD = 'domain_name'
+VOUCH_MESSAGE_FIELD = 'message'
+
+
+### Crypto Constants ###
+
+BOX_NONCE_BYTES = 24
+
 SCRYPT_N = Math.pow(2, 14)
 SCRYPT_R = 8
 SCRYPT_P = 1
 SCRYPT_L = 32
+
 
 nacl = nacl_factory.instantiate()
 scrypt = scrypt_module_factory()
@@ -16,17 +44,140 @@ credentialsToSecretKey = (email, password) ->
   scrypt.crypto_scrypt(password_hash, encode_utf8(email),
     SCRYPT_N, SCRYPT_R, SCRYPT_P, SCRYPT_L)
 
-b64encode = (x) ->
-  btoa String.fromCharCode.apply(null, x)
 
+concatBuffers = (x, y) ->
+  z = new Uint8Array(x.byteLength + y.byteLength);
+  z.set new Uint8Array(x), 0
+  z.set new Uint8Array(y), x.byteLength
+  z
+
+
+b64encode = (arr) ->
+  base64Str = btoa String.fromCharCode.apply(null, arr)
+  base64Str.replace(/\+/g, '-').replace(/\//g, '_')
+
+
+b64decode = (base64Str) ->
+  base64DecToArr base64Str.replace(/-/g, '+').replace(/_/g, '/')
+
+
+sendHello = (transientKeys, success, error) ->
+  serverPublicKey = b64decode SERVER_PUBLIC_KEY
+  zeros = new Uint8Array(HELLO_PADDING_BYTES)
+  nonce = nacl.crypto_box_random_nonce()
+  zerosBox = nacl.crypto_box zeros, nonce, serverPublicKey, transientKeys.boxSk
+  noncedZerosBox = concatBuffers nonce, zerosBox
+
+  payload = {}
+  payload[HELLO_CLIENT_TRANSIENT_PKEY_FIELD] = b64encode transientKeys.boxPk
+  payload[HELLO_PADDING_FIELD] = b64encode Uint8Array(HELLO_PADDING_BYTES)
+  payload[HELLO_ZEROS_BOX_FIELD] = b64encode noncedZerosBox
+  $.ajax
+    type: 'POST'
+    url: HELLO_URL
+    data: JSON.stringify payload
+    contentType: 'application/json'
+    dataType: 'json'
+    error: (xhr) -> error xhr
+    success: (data, status, xhr) ->
+      cookie_box = b64decode data.cookie_box
+      cookie_box_nonce = cookie_box.subarray 0, BOX_NONCE_BYTES
+      cookie_box_cipher = cookie_box.subarray BOX_NONCE_BYTES
+      cookie = JSON.parse decode_utf8 nacl.crypto_box_open(
+        cookie_box_cipher, cookie_box_nonce, serverPublicKey, transientKeys.boxSk)
+      success b64decode(cookie.server_tpkey), cookie.cookie
+
+
+sendInitiate = (userKeys, transientKeys, serverTPKey,
+  cookie, message, success, error) ->
+  serverPublicKey = b64decode SERVER_PUBLIC_KEY
+
+  transientKeyNonce = nacl.crypto_box_random_nonce()
+  transientKeyBox = nacl.crypto_box(
+    transientKeys.boxPk, transientKeyNonce, serverPublicKey, userKeys.boxSk)
+  noncedTransientKeyBox = concatBuffers transientKeyNonce, transientKeyBox
+
+  vouch = {}
+  vouch[VOUCH_CLIENT_PKEY_FIELD] = b64encode userKeys.boxPk
+  vouch[VOUCH_TRANSIENT_KEY_BOX_FIELD] = b64encode noncedTransientKeyBox
+  vouch[VOUCH_DOMAIN_NAME_FIELD] = SERVER_DOMAIN_NAME
+  vouch[VOUCH_MESSAGE_FIELD] = message
+  vouchBuffer = encode_utf8 JSON.stringify vouch
+
+  vouchNonce = nacl.crypto_box_random_nonce()
+  vouchBox = nacl.crypto_box(
+    vouchBuffer, vouchNonce, serverTPKey, transientKeys.boxSk)
+  noncedVouchBox = concatBuffers vouchNonce, vouchBox
+
+  payload = {}
+  payload[INITIATE_CLIENT_TRANSIENT_PKEY_FIELD] = b64encode transientKeys.boxPk
+  payload[INITIATE_COOKIE_FIELD] = cookie
+  payload[INITIATE_VOUCH_FIELD] = b64encode noncedVouchBox
+  $.ajax
+    type: 'POST'
+    url: INITIATE_URL
+    data: JSON.stringify payload
+    contentType: 'application/json'
+    dataType: 'json'
+    error: (xhr) -> error xhr
+    success: (data, status, xhr) ->
+      response_box = b64decode data.response
+      response_box_nonce = response_box.subarray 0, BOX_NONCE_BYTES
+      response_box_cipher = response_box.subarray BOX_NONCE_BYTES
+      response = decode_utf8 nacl.crypto_box_open(
+        response_box_cipher, response_box_nonce, serverTPKey, transientKeys.boxSk)
+      success response
+
+
+sendMessage = (userKeys, message) ->
+  transientKeys = nacl.crypto_box_keypair()
+  serverPublicKey = b64decode SERVER_PUBLIC_KEY
+  sendHello transientKeys,
+    (serverTPKey, cookie) ->
+      console.log serverTPKey
+      console.log cookie
+      message = {'alabama': 123}
+      sendInitiate(userKeys, transientKeys, serverTPKey, cookie, message,
+        (response) -> console.log response
+        (xhr) -> alert xhr.responseText
+      )
+    (xhr) -> alert xhr.responseText
+
+
+retrieveUserData = (userKeys, success, error) ->
+  transientKeys = nacl.crypto_box_keypair()
+  serverPublicKey = b64decode SERVER_PUBLIC_KEY
+
+  zeros = new Uint8Array(HELLO_PADDING_BYTES)
+  nonce = nacl.crypto_box_random_nonce()
+  zerosBox = nacl.crypto_box zeros, nonce, serverPublicKey, transientKeys.boxSk
+  noncedZerosBox = concatBuffers nonce, zerosBox
+  $.ajax
+    type: 'POST'
+    url: HELLO_URL
+    data: JSON.stringify
+      client_tpkey: b64encode transientKeys.boxPk
+      padding: b64encode Uint8Array(HELLO_PADDING_BYTES)
+      zeros_box: b64encode noncedZerosBox
+    contentType: 'application/json'
+    dataType: 'json'
+    error: -> alert(
+      'Auch! The source cannot be created at the moment. ' +
+      'Please try again or contact us at support@reinfer.io')
+    success: (data, status, xhr) -> success(data)
 
 DisturbeApp = React.createClass
   getInitialState: ->
     userKeys: null
 
   setPrivateKey: (privateKey) ->
-    userKeys = nacl.crypto_box_keypair_from_raw_sk(privateKey)
+    userKeys = nacl.crypto_box_keypair_from_raw_sk privateKey
     this.setState userKeys: userKeys
+    #retrieveUserData userKeys, this.setUserData, (error) -> alert error
+    sendMessage userKeys
+
+  setUserData: (userData) ->
+    console.log userData
 
   render: ->
     div null,
@@ -44,11 +195,13 @@ KeyCabinet = React.createClass
           InputField
             type: 'text'
             label: span className: 'text-monospace', 'Public Key'
-            value: b64encode(this.props.userKeys.boxPk)
+            inputClass: 'text-monospace'
+            value: b64encode this.props.userKeys.boxPk
           InputField
             type: 'text'
             label: span className: 'text-monospace', 'Secret Key'
-            value: b64encode(this.props.userKeys.boxSk)
+            inputClass: 'text-monospace'
+            value: b64encode this.props.userKeys.boxSk
 
 
 GeneratePrivateKey = React.createClass
@@ -69,10 +222,12 @@ GeneratePrivateKey = React.createClass
           InputField
             type: 'text'
             label: span className: 'text-monospace', 'Email address'
+            inputClass: 'text-monospace'
             onChange: ((email) -> this.setState email: email).bind this
           InputField
             type: 'password',
             label: span className: 'text-monospace', 'Password'
+            inputClass: 'text-monospace'
             onChange: ((password) -> this.setState password: password).bind this
       div className: 'form-group',
         div className: 'col-md-12 ',
@@ -100,7 +255,8 @@ InputField = React.createClass
       className: 'form-control'
       onChange: this.onChange
     if this.props.value? then inputProps.value = this.props.value
-    console.log inputProps.value
+    if this.props.inputClass?
+      inputProps.className += ' ' + this.props.inputClass
 
     div className: 'form-group',
       div className: 'col-md-12',
